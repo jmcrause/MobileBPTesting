c1a578d32ecc8627a2b0eb768bc0eb66e9945684
jmcrause <jmcrause@gmail.com>
2021-07-10 21:24:47 +0200

    Implemented graph (with sinusoidal test), Raising edge heart rate calculation.

--- app/src/main/java/com/example/mobilebptesting/MeasureBP.java
+++ app/src/main/java/com/example/mobilebptesting/MeasureBP.java
@@ -3,9 +3,11 @@ package com.example.mobilebptesting;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -32,13 +34,16 @@ public class MeasureBP extends AppCompatActivity implements View.OnClickListener
     private LineGraphSeries<DataPoint> series1;
     ImageView imageView;
     Button btn_start;
+    TextView mTextField;
+    CountDownTimer countDownTimer, graphTimer;
 
     double [] x_arr = new double[30*20];
     double [] y_arr = new double[30*20];
     int frame_count = 0;
-    String id, sbp_ref, dbp_ref, hr_ref;
+    String id, sbp_ref, dbp_ref, hr_ref, sbp_app, dbp_app, hr_app;
     Long time_start;
     int attempts = 1;
+    double reference_line = 0.5;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,6 +52,8 @@ public class MeasureBP extends AppCompatActivity implements View.OnClickListener
         setContentView(R.layout.measure_bp);
 
         time_start = System.currentTimeMillis()/1000;
+
+        mTextField = findViewById(R.id.textViewMeasure);
 
         Bundle bundle = getIntent().getExtras();
         id = bundle.getString("id");
@@ -81,6 +88,46 @@ public class MeasureBP extends AppCompatActivity implements View.OnClickListener
         series1.setColor(R.color.design_default_color_primary);
 
         graph.addSeries(series1);
+
+        //Create 20 CountDownTimer that refreshes screen every 100 ms
+        countDownTimer = new CountDownTimer(20000, 33) {
+
+            public void onTick(long millisUntilFinished) {
+                addFrame(millisUntilFinished);
+            }
+
+            public void onFinish() {
+            }
+        };
+
+
+        graphTimer = new CountDownTimer(20000, 100) {
+
+            public void onTick(long millisUntilFinished) {
+
+                if (millisUntilFinished > 15000) {
+                    mTextField.setText(getText(R.string.calibrating) + " " + (millisUntilFinished / 1000 - 15) + "s");
+                }
+                else {
+                    mTextField.setText("HR: " + calculateHeartRate() + " BPM");
+                }
+                if (millisUntilFinished == 15000) {
+                    reference_line = centerValue ();
+                }
+
+                series1.resetData(generateData());
+            }
+
+            public void onFinish() {
+                sbp_app = sbp_ref;
+                dbp_app = dbp_ref;
+                hr_app = String.valueOf(calculateHeartRate());
+
+                mTextField.setText("Done!");
+                frame_count = 0;
+                measurementComplete();
+            }
+        };
     }
 
     //Data transferred to Google Drive
@@ -183,9 +230,9 @@ public class MeasureBP extends AppCompatActivity implements View.OnClickListener
         intent.putExtra("sbp_ref", sbp_ref);
         intent.putExtra("dbp_ref", dbp_ref);
         intent.putExtra("hr_ref", hr_ref);
-        intent.putExtra("sbp_app", sbp_ref);
-        intent.putExtra("dbp_app", dbp_ref);
-        intent.putExtra("hr_app", hr_ref);
+        intent.putExtra("sbp_app", sbp_app);
+        intent.putExtra("dbp_app", dbp_app);
+        intent.putExtra("hr_app", hr_app);
         startActivity(intent);
     }
 
@@ -196,9 +243,16 @@ public class MeasureBP extends AppCompatActivity implements View.OnClickListener
             startActivity(intent);
         }
         if (v == btn_start){
-            measurementComplete();
+            countDownTimer.start();
+            graphTimer.start();
         }
 
+    }
+
+    private void addFrame(long millisUntilFinished) {
+        x_arr[frame_count] = (20000-millisUntilFinished)/1000.0;
+        y_arr[frame_count] = (Math.sin(x_arr[frame_count]*2*3.14)+1)/2.0;
+        frame_count++;
     }
 
     private DataPoint[] generateData() {
@@ -225,11 +279,55 @@ public class MeasureBP extends AppCompatActivity implements View.OnClickListener
     private String arrayToString () {
         String arr_str = "";
 
-        for (int i = 0; i < y_arr.length; i++)
+        for (int i = 0; i < frame_count; i++)
         {
-            arr_str = arr_str + ", " + i;
+            arr_str = arr_str + ", " + y_arr[i];
         }
 
         return arr_str;
+    }
+
+    private double centerValue () {
+        double center = 0;
+        // Find Min and Max values
+        double min = 1;
+        double max = 0;
+        for (int i = 0; i < frame_count; i++)
+        {
+            if (y_arr[i] > max) {
+                max = y_arr[i];
+            }
+            if (y_arr[i] < min) {
+                min = y_arr[i];
+            }
+        }
+
+        center = (max - min)/2.0;
+
+        return center;
+    }
+
+    private int calculateHeartRate () {
+        int rolling_hr = 60;
+
+        // Count rising edges
+        double count = 0;
+        int start = 0;
+        int end = 0;
+        for (int i = 1; i < frame_count; i++)
+        {
+            if (y_arr[i] >= reference_line && y_arr[i-1] < reference_line) {
+                count++;
+                if (count==1) {
+                    start = i;
+                }
+                end = i;
+            }
+        }
+
+        double dt = (end - start)/30.0;
+        rolling_hr = (int) Math.round((count-1)/dt * 60);
+
+        return rolling_hr;
     }
 }
